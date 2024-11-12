87e1c14f60499d3c0050fe44cc188ac1c3163721
michkied <michkied@gmail.com>
2024-11-12 23:37:40 +0100

    draw surface of points

--- MainWindow.cs
+++ MainWindow.cs
@@ -1,6 +1,8 @@
 using System.Drawing;
 using System.Net;
 using System.Numerics;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Policy;
 using System.Windows.Forms;
 
 namespace BezierSurface
@@ -9,54 +11,139 @@ namespace BezierSurface
     {
         private List<Triangle> _mesh = new();
         private Bitmap? _bmp;
-        private List<Vector3> vertices = new();
+        private List<Vector3> controlPoints = new();
+        private List<Vertex> vertices = new();
 
-        private double alpha = 30 / Math.PI * .5;
-        private double beta = 5 / Math.PI * .5;
+        private double alpha = 45 / Math.PI * .5;
+        private double beta = 7 / Math.PI * .5;
+
+        private int precision = 50;
 
         public MainWindow()
         {
             InitializeComponent();
             LoadData();
+            GenerateVerticies();
             RotateVerticies();
             DrawBitmap();
         }
 
         private void LoadData()
         {
-            vertices = new List<Vector3>
+            controlPoints = new List<Vector3>
             {
-                new Vector3(-120, 120, 0),
+                new Vector3(-120, 120, -20),
                 new Vector3(-40, 120, 0),
                 new Vector3(40, 120, 0),
-                new Vector3(120, 120, 0),
+                new Vector3(120, 120, -20),
                 new Vector3(-120, 40, 0),
-                new Vector3(-40, 40, 0),
-                new Vector3(40, 40, 0),
+                new Vector3(-40, 40, 20),
+                new Vector3(40, 40, 20),
                 new Vector3(120, 40, 0),
                 new Vector3(-120, -40, 0),
-                new Vector3(-40, -40, 0),
-                new Vector3(40, -40, 0),
+                new Vector3(-40, -40, 20),
+                new Vector3(40, -40, 20),
                 new Vector3(120, -40, 0),
-                new Vector3(-120, -120, 0),
+                new Vector3(-120, -120, -20),
                 new Vector3(-40, -120, 0),
                 new Vector3(40, -120, 0),
-                new Vector3(120, -120, 0)
+                new Vector3(120, -120, -20)
             };
+        }
+
+        private void GenerateVerticies()
+        {
+            List<Vector3> tempControlPoints = new();
+            List<Vector3> surfacePoints = new();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Vector3 start = controlPoints[i * 4];
+                Vector3 control1 = controlPoints[i * 4 + 1];
+                Vector3 control2 = controlPoints[i * 4 + 2];
+                Vector3 end = controlPoints[i * 4 + 3];
+
+                GenerateCurve(start, control1, control2, end, tempControlPoints);
+            }
+
+            for (int i = 0; i < precision; i++)
+            {
+                Vector3 start = tempControlPoints[i];
+                Vector3 control1 = tempControlPoints[i + precision];
+                Vector3 control2 = tempControlPoints[i + 2 * precision];
+                Vector3 end = tempControlPoints[i + 3 * precision];
+
+                GenerateCurve(start, control1, control2, end, surfacePoints);
+            }
+
+            foreach (var point in surfacePoints)
+            {
+                vertices.Add(new Vertex { P = point });
+            }
+        }
+
+        private void GenerateCurve(Vector3 start, Vector3 control1, Vector3 control2, Vector3 end, List<Vector3> outList)
+        {
+            int numOfPoints = precision;
+            double d = 1.0 / (double)numOfPoints;
+            double d2 = d * d;
+            double d3 = d2 * d;
+
+            double A0X = start.X;
+            double A0Y = start.Y;
+            double A0Z = start.Z;
+
+            double A1X = 3 * (control1.X - start.X);
+            double A1Y = 3 * (control1.Y - start.Y);
+            double A1Z = 3 * (control1.Z - start.Z);
+
+            double A2X = 3 * (control2.X - 2 * control1.X + start.X);
+            double A2Y = 3 * (control2.Y - 2 * control1.Y + start.Y);
+            double A2Z = 3 * (control2.Z - 2 * control1.Z + start.Z);
+
+            double A3X = end.X - 3 * control2.X + 3 * control1.X - start.X;
+            double A3Y = end.Y - 3 * control2.Y + 3 * control1.Y - start.Y;
+            double A3Z = end.Z - 3 * control2.Z + 3 * control1.Z - start.Z;
+
+            double prevP0X = A0X;
+            double prevP0Y = A0Y;
+            double prevP0Z = A0Z;
+            double prevP1X = A3X * d3 + A2X * d2 + A1X * d;
+            double prevP1Y = A3Y * d3 + A2Y * d2 + A1Y * d;
+            double prevP1Z = A3Z * d3 + A2Z * d2 + A1Z * d;
+            double prevP2X = 6 * A3X * d3 + 2 * A2X * d2;
+            double prevP2Y = 6 * A3Y * d3 + 2 * A2Y * d2;
+            double prevP2Z = 6 * A3Z * d3 + 2 * A2Z * d2;
+
+            outList.Add(new Vector3((float)A0X, (float)A0Y, (float)A0Z));
+
+            for (int step = 1; step < numOfPoints; step++)
+            {
+                prevP0X = prevP0X + prevP1X;
+                prevP0Y = prevP0Y + prevP1Y;
+                prevP0Z = prevP0Z + prevP1Z;
+
+                prevP1X = prevP1X + prevP2X;
+                prevP1Y = prevP1Y + prevP2Y;
+                prevP1Z = prevP1Z + prevP2Z;
+
+                prevP2X = prevP2X + 6 * A3X * d3;
+                prevP2Y = prevP2Y + 6 * A3Y * d3;
+                prevP2Z = prevP2Z + 6 * A3Z * d3;
+
+                outList.Add(new Vector3((float)prevP0X, (float)prevP0Y, (float)prevP0Z));
+            }
         }
 
         private void RotateVerticies()
         {
-            List<Vector3> newVerticies = new();
             Matrix4x4 rotMatrixZ = Matrix4x4.CreateRotationZ((float)alpha);
             Matrix4x4 rotMatrixX = Matrix4x4.CreateRotationX((float)beta);
 
             foreach (var vertex in vertices)
             {
-                newVerticies.Add(Vector3.Transform(Vector3.Transform(vertex, rotMatrixZ), rotMatrixX));
+                vertex.P = Vector3.Transform(Vector3.Transform(vertex.P, rotMatrixZ), rotMatrixX);
             }
-
-            vertices = newVerticies;
         }
 
         private void DrawBitmap()
@@ -71,29 +158,11 @@ namespace BezierSurface
 
             foreach (var vertex in vertices)
             {
-                g.FillEllipse(Brushes.Black, vertex.X, vertex.Y, 10, 10);
+                g.FillRectangle(Brushes.Black, vertex.P.X, vertex.P.Y, 2, 2);
             }
 
 
             mainPictureBox.Image = _bmp;
-        }
-
-        private void mainPictureBox_Paint(object sender, PaintEventArgs e)
-        {
-            Graphics g = e.Graphics;
-
-
-            Pen pen = new Pen(Color.Black, 2); // Line color and thickness
-
-            // Define start and end points of the line
-            Point startPoint = new Point(10, 10);
-            Point endPoint = new Point(100, 100);
-
-            // Draw the line
-            g.DrawLine(pen, startPoint, endPoint);
-
-            //// Dispose of the Pen after use
-            pen.Dispose();
         }
     }
 }
