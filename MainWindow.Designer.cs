d3baa48870cfcc7587f2f9ab9fa9c5c2348eacf1
michkied <michkied@gmail.com>
2024-11-13 14:45:36 +0100

    add sliders and tangent calculation

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -30,7 +30,14 @@
         {
             mainPictureBox = new PictureBox();
             PropertiesBox = new GroupBox();
+            betaSlider = new TrackBar();
+            alphaSlider = new TrackBar();
+            precisionSlider = new TrackBar();
             ((System.ComponentModel.ISupportInitialize)mainPictureBox).BeginInit();
+            PropertiesBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)betaSlider).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)alphaSlider).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)precisionSlider).BeginInit();
             SuspendLayout();
             // 
             // mainPictureBox
@@ -45,6 +52,9 @@
             // PropertiesBox
             // 
             PropertiesBox.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            PropertiesBox.Controls.Add(betaSlider);
+            PropertiesBox.Controls.Add(alphaSlider);
+            PropertiesBox.Controls.Add(precisionSlider);
             PropertiesBox.Dock = DockStyle.Right;
             PropertiesBox.Location = new Point(500, 0);
             PropertiesBox.Margin = new Padding(10);
@@ -53,6 +63,35 @@
             PropertiesBox.TabIndex = 1;
             PropertiesBox.TabStop = false;
             PropertiesBox.Text = "Properties";
+            // 
+            // betaSlider
+            // 
+            betaSlider.Location = new Point(6, 185);
+            betaSlider.Name = "betaSlider";
+            betaSlider.Size = new Size(296, 45);
+            betaSlider.TabIndex = 4;
+            betaSlider.Scroll += betaSlider_Scroll;
+            // 
+            // alphaSlider
+            // 
+            alphaSlider.Location = new Point(6, 105);
+            alphaSlider.Maximum = 45;
+            alphaSlider.Minimum = -45;
+            alphaSlider.Name = "alphaSlider";
+            alphaSlider.Size = new Size(296, 45);
+            alphaSlider.TabIndex = 3;
+            alphaSlider.Scroll += alphaSlider_Scroll;
+            // 
+            // precisionSlider
+            // 
+            precisionSlider.Location = new Point(6, 39);
+            precisionSlider.Maximum = 50;
+            precisionSlider.Minimum = 3;
+            precisionSlider.Name = "precisionSlider";
+            precisionSlider.Size = new Size(296, 45);
+            precisionSlider.TabIndex = 2;
+            precisionSlider.Value = 4;
+            precisionSlider.Scroll += precisionSlider_Scroll;
             // 
             // MainWindow
             // 
@@ -64,6 +103,11 @@
             Name = "MainWindow";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)mainPictureBox).EndInit();
+            PropertiesBox.ResumeLayout(false);
+            PropertiesBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)betaSlider).EndInit();
+            ((System.ComponentModel.ISupportInitialize)alphaSlider).EndInit();
+            ((System.ComponentModel.ISupportInitialize)precisionSlider).EndInit();
             ResumeLayout(false);
         }
 
@@ -71,5 +115,8 @@
 
         private PictureBox mainPictureBox;
         private GroupBox PropertiesBox;
+        private TrackBar betaSlider;
+        private TrackBar alphaSlider;
+        private TrackBar precisionSlider;
     }
 }
--- MainWindow.cs
+++ MainWindow.cs
@@ -13,10 +13,10 @@ namespace BezierSurface
         private Bitmap? _bmp;
         private List<Vector3> controlPoints = new();
 
-        private double alpha = 45 / Math.PI * .5;
-        private double beta = 7 / Math.PI * .5;
+        private double alpha = 0 / Math.PI * .5;
+        private double beta = 0 / Math.PI * .5;
 
-        private int precision = 10;
+        private int precision = 5;
 
         public MainWindow()
         {
@@ -30,54 +30,86 @@ namespace BezierSurface
         {
             controlPoints = new List<Vector3>
             {
-                new Vector3(-120, 120, -20),
+                new Vector3(-120, 120, 30),
                 new Vector3(-40, 120, 0),
                 new Vector3(40, 120, 0),
-                new Vector3(120, 120, -20),
+                new Vector3(120, 120, 0),
                 new Vector3(-120, 40, 0),
-                new Vector3(-40, 40, 20),
-                new Vector3(40, 40, 20),
+                new Vector3(-40, 40, 0),
+                new Vector3(40, 40, 0),
                 new Vector3(120, 40, 0),
                 new Vector3(-120, -40, 0),
-                new Vector3(-40, -40, 20),
-                new Vector3(40, -40, 20),
+                new Vector3(-40, -40, 0),
+                new Vector3(40, -40, 0),
                 new Vector3(120, -40, 0),
-                new Vector3(-120, -120, -20),
+                new Vector3(-120, -120, 0),
                 new Vector3(-40, -120, 0),
                 new Vector3(40, -120, 0),
-                new Vector3(120, -120, -20)
+                new Vector3(120, -120, 0)
             };
         }
 
         private void GenerateVerticies()
         {
-            List<Vector3> tempControlPoints = new();
-            List<Vector3> surfacePoints = new();
+            List<Vector3> tempControlPointsX = new();
+            List<Vector3> tempControlPointsY = new();
 
             for (int i = 0; i < 4; i++)
             {
-                Vector3 start = controlPoints[i * 4];
-                Vector3 control1 = controlPoints[i * 4 + 1];
-                Vector3 control2 = controlPoints[i * 4 + 2];
-                Vector3 end = controlPoints[i * 4 + 3];
+                GenerateCurve(
+                    controlPoints[i * 4],
+                    controlPoints[i * 4 + 1],
+                    controlPoints[i * 4 + 2],
+                    controlPoints[i * 4 + 3],
+                    tempControlPointsX
+                );
 
-                GenerateCurve(start, control1, control2, end, tempControlPoints);
+                GenerateCurve(
+                    controlPoints[i],
+                    controlPoints[i + 4],
+                    controlPoints[i + 8],
+                    controlPoints[i + 12],
+                    tempControlPointsY
+                );
             }
+
+            List<Vector3> surfacePoints = new();
+            List<Vector3> tangentsU = new();
+            List<Vector3> tangentsV = new();
 
             for (int i = 0; i < precision; i++)
             {
-                Vector3 start = tempControlPoints[i];
-                Vector3 control1 = tempControlPoints[i + precision];
-                Vector3 control2 = tempControlPoints[i + 2 * precision];
-                Vector3 end = tempControlPoints[i + 3 * precision];
+                GenerateCurve(
+                    tempControlPointsX[i],
+                    tempControlPointsX[i + precision],
+                    tempControlPointsX[i + 2 * precision],
+                    tempControlPointsX[i + 3 * precision],
+                    surfacePoints,
+                    tangentsV
+                );
 
-                GenerateCurve(start, control1, control2, end, surfacePoints);
+                GenerateCurve(
+                    tempControlPointsY[i],
+                    tempControlPointsY[i + precision],
+                    tempControlPointsY[i + 2 * precision],
+                    tempControlPointsY[i + 3 * precision],
+                    null,
+                    tangentsU
+                );
             }
 
             List<Vertex> vertices = new();
-            foreach (var point in surfacePoints)
+            for (int i = 0; i < surfacePoints.Count; i++)
             {
-                vertices.Add(new Vertex { P = point });
+                vertices.Add(
+                    new Vertex
+                    {
+                        P = surfacePoints[i],
+                        Pu = tangentsU[i],
+                        Pv = tangentsV[i],
+                        N = Vector3.Normalize(Vector3.Cross(tangentsU[i], tangentsV[i])),
+                    }
+                );
             }
 
             RotateVerticies(vertices);
@@ -97,61 +129,42 @@ namespace BezierSurface
             }
         }
 
-        private void GenerateCurve(Vector3 start, Vector3 control1, Vector3 control2, Vector3 end, List<Vector3> outList)
+        private void GenerateCurve(Vector3 start, Vector3 control1, Vector3 control2, Vector3 end, List<Vector3>? points, List<Vector3>? tangents = null)
         {
             int numOfPoints = precision;
-            double d = 1.0 / (double)numOfPoints;
-            double d2 = d * d;
-            double d3 = d2 * d;
+            float d = 1.0f / (float)numOfPoints;
+            float d2 = d * d;
+            float d3 = d2 * d;
 
-            // TODO: use vectors
+            Vector3 A0 = start;
+            Vector3 A1 = 3 * (control1 - start);
+            Vector3 A2 = 3 * (control2 - 2 * control1 + start);
+            Vector3 A3 = end - 3 * control2 + 3 * control1 - start;
 
-            double A0X = start.X;
-            double A0Y = start.Y;
-            double A0Z = start.Z;
+            Vector3 nextP0 = A0;
+            Vector3 nextP1 = A3 * d3 + A2 * d2 + A1 * d;
+            Vector3 nextP2 = 6 * A3 * d3 + 2 * A2 * d2;
 
-            double A1X = 3 * (control1.X - start.X);
-            double A1Y = 3 * (control1.Y - start.Y);
-            double A1Z = 3 * (control1.Z - start.Z);
-
-            double A2X = 3 * (control2.X - 2 * control1.X + start.X);
-            double A2Y = 3 * (control2.Y - 2 * control1.Y + start.Y);
-            double A2Z = 3 * (control2.Z - 2 * control1.Z + start.Z);
-
-            double A3X = end.X - 3 * control2.X + 3 * control1.X - start.X;
-            double A3Y = end.Y - 3 * control2.Y + 3 * control1.Y - start.Y;
-            double A3Z = end.Z - 3 * control2.Z + 3 * control1.Z - start.Z;
-
-            double prevP0X = A0X;
-            double prevP0Y = A0Y;
-            double prevP0Z = A0Z;
-            double prevP1X = A3X * d3 + A2X * d2 + A1X * d;
-            double prevP1Y = A3Y * d3 + A2Y * d2 + A1Y * d;
-            double prevP1Z = A3Z * d3 + A2Z * d2 + A1Z * d;
-            double prevP2X = 6 * A3X * d3 + 2 * A2X * d2;
-            double prevP2Y = 6 * A3Y * d3 + 2 * A2Y * d2;
-            double prevP2Z = 6 * A3Z * d3 + 2 * A2Z * d2;
+            Vector3 nextPt0 = A1;
+            Vector3 nextPt1 = 3 * A3 * d2 + 2 * A2 * d;
 
             // u = 0
             // v = arg
-            outList.Add(new Vector3((float)A0X, (float)A0Y, (float)A0Z));
+            points?.Add(nextP0);
+            tangents?.Add(Vector3.Normalize(nextPt0));
 
             for (int step = 1; step < numOfPoints; step++)
             {
-                prevP0X = prevP0X + prevP1X;
-                prevP0Y = prevP0Y + prevP1Y;
-                prevP0Z = prevP0Z + prevP1Z;
+                nextP0 += nextP1;
+                nextP1 += nextP2;
+                nextP2 += 6 * A3 * d3;
 
-                prevP1X = prevP1X + prevP2X;
-                prevP1Y = prevP1Y + prevP2Y;
-                prevP1Z = prevP1Z + prevP2Z;
-
-                prevP2X = prevP2X + 6 * A3X * d3;
-                prevP2Y = prevP2Y + 6 * A3Y * d3;
-                prevP2Z = prevP2Z + 6 * A3Z * d3;
+                nextPt0 += nextPt1;
+                nextPt1 += 6 * A3 * d2;
 
                 // u += d
-                outList.Add(new Vector3((float)prevP0X, (float)prevP0Y, (float)prevP0Z));
+                points?.Add(nextP0);
+                tangents?.Add(Vector3.Normalize(nextPt0));
             }
         }
 
@@ -162,7 +175,10 @@ namespace BezierSurface
 
             foreach (var vertex in vertices)
             {
-                vertex.P = Vector3.Transform(Vector3.Transform(vertex.P, rotMatrixZ), rotMatrixX);
+                vertex.P_rotated = Vector3.Transform(Vector3.Transform(vertex.P, rotMatrixZ), rotMatrixX);
+                vertex.Pu_rotated = Vector3.Transform(Vector3.Transform(vertex.Pu, rotMatrixZ), rotMatrixX);
+                vertex.Pv_rotated = Vector3.Transform(Vector3.Transform(vertex.Pv, rotMatrixZ), rotMatrixX);
+                vertex.N_rotated = Vector3.Transform(Vector3.Transform(vertex.N, rotMatrixZ), rotMatrixX);
             }
         }
 
@@ -178,13 +194,37 @@ namespace BezierSurface
 
             foreach (var triangle in _mesh)
             {
-                g.DrawLine(Pens.Black, triangle.v1.P.X, triangle.v1.P.Y, triangle.v2.P.X, triangle.v2.P.Y);
-                g.DrawLine(Pens.Black, triangle.v2.P.X, triangle.v2.P.Y, triangle.v3.P.X, triangle.v3.P.Y);
-                g.DrawLine(Pens.Black, triangle.v3.P.X, triangle.v3.P.Y, triangle.v1.P.X, triangle.v1.P.Y);
+                g.DrawLine(Pens.Black, triangle.v1.P_rotated.X, triangle.v1.P_rotated.Y, triangle.v2.P_rotated.X, triangle.v2.P_rotated.Y);
+                g.DrawLine(Pens.Black, triangle.v2.P_rotated.X, triangle.v2.P_rotated.Y, triangle.v3.P_rotated.X, triangle.v3.P_rotated.Y);
+                g.DrawLine(Pens.Black, triangle.v3.P_rotated.X, triangle.v3.P_rotated.Y, triangle.v1.P_rotated.X, triangle.v1.P_rotated.Y);
             }
 
 
             mainPictureBox.Image = _bmp;
+        }
+
+        private void precisionSlider_Scroll(object sender, EventArgs e)
+        {
+            precision = precisionSlider.Value;
+            _mesh.Clear();
+            GenerateVerticies();
+            DrawBitmap();
+        }
+
+        private void alphaSlider_Scroll(object sender, EventArgs e)
+        {
+            alpha = alphaSlider.Value / Math.PI * .5;
+            _mesh.Clear();
+            GenerateVerticies();
+            DrawBitmap();
+        }
+
+        private void betaSlider_Scroll(object sender, EventArgs e)
+        {
+            beta = betaSlider.Value / Math.PI * .5;
+            _mesh.Clear();
+            GenerateVerticies();
+            DrawBitmap();
         }
     }
 }
